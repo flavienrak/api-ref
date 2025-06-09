c1caadbc5647635a7bf74a1cf78f08787bb57a6f
Randriantahina <shanjeev24.apollon@gmail.com>
2025-06-09 08:57:30 +0300

    feat:add room controller

---
+++ src/controllers/room.controller.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from 'express';
+import prisma from '@/lib/db';
+import jwt from 'jsonwebtoken';
+
+const secretKey = process.env.JWT_SECRET_KEY as string;
+const tokenName = process.env.AUTH_TOKEN_NAME as string;
+
+export const room = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { name }: { name: string } = req.body;
+    const token = req.cookies?.[tokenName];
+    if (!name) {
+      res.json({ error: 'Le nom est requis' });
+      return;
+    }
+    if (!token) {
+      res.json({ error: 'Non authentifié' });
+      return;
+    }
+    const decoded = jwt.verify(token, secretKey) as { infos: { id: string } };
+    const userId = Number(decoded.infos.id);
+    if (!userId) {
+      res.json({ error: 'Token invalide' });
+      return;
+    }
+
+    const existingRoom = await prisma.room.findMany({ where: { name } });
+    if (existingRoom) {
+      res.json({ roomAlreadyExists: true });
+      return;
+    }
+
+    const newRoom = await prisma.room.create({
+      data: { name, userId },
+    });
+
+    res.json({ id: newRoom.id });
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur lors de la création de la room' });
+  }
+};
