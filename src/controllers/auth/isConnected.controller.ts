54eaaed2fcda0105f30e9f54c90942ad9781882a
Randriantahina <shanjeev24.apollon@gmail.com>
2025-06-09 08:21:40 +0300

    fix:imporve isconnected.controller

--- src/controllers/auth/isConnected.controller.ts
+++ src/controllers/auth/isConnected.controller.ts
@@ -1,6 +1,7 @@
 import prisma from '@/lib/db';
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
+
 const secretKey = process.env.JWT_SECRET_KEY as string;
 const tokenName = process.env.AUTH_TOKEN_NAME as string;
 
@@ -8,27 +9,19 @@ export const checkConnectionStatus = async (
   req: Request,
   res: Response,
 ): Promise<void> => {
+  const token = req.cookies?.[tokenName];
+
+  if (!token) {
+    res.json({ noToken: true });
+    return;
+  }
+
   try {
-    console.log('tokenName:', tokenName);
-    const token = req.cookies?.[tokenName];
-    console.log('token:', token);
-
-    if (!token) {
-      res.json({ noToken: true });
-      return;
-    }
-
     const decoded = jwt.verify(token, secretKey) as { infos: { id: string } };
 
-    let user;
-    try {
-      user = await prisma.user.findUnique({
-        where: { id: Number(decoded.infos.id) },
-      });
-    } catch (error) {
-      res.status(500).json({ error });
-      return;
-    }
+    const user = await prisma.user.findUnique({
+      where: { id: Number(decoded.infos.id) },
+    });
 
     if (!user) {
       res.clearCookie(tokenName, {
@@ -36,16 +29,16 @@ export const checkConnectionStatus = async (
         secure: true,
         sameSite: 'none',
       });
-
       res.json({ userNotFound: true });
       return;
     }
+
     res.json({ id: user.id });
   } catch (error: any) {
     if (error.name === 'TokenExpiredError') {
       res.json({ expired: true });
     } else {
-      res.status(500).json(error);
+      res.status(500).json({ error: error.message });
     }
   }
 };
