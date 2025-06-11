131ef5f3e81b293deef2dbb1d8e6dfbee7cd1380
Randriantahina <shanjeev24.apollon@gmail.com>
2025-06-11 19:10:29 +0300

    feat(token):add new controller for google oauth

--- src/controllers/auth/verifyGoogleOauth.controller.ts
+++ src/controllers/auth/verifyGoogleOauth.controller.ts
@@ -1,28 +1,8 @@
 import prisma from '@/lib/db';
 import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
+
 const secretKey = process.env.JWT_SECRET_KEY as string;
-
-export const verifyToken = (req: Request, res: Response): void => {
-  try {
-    const { token } = req.params;
-
-    if (!token) {
-      res.json({ message: 'Aucun token fourni' });
-      return;
-    }
-
-    jwt.verify(token, secretKey);
-
-    res.status(200).json({ decoded: true });
-  } catch (error: any) {
-    if (error.name === 'TokenExpiredError') {
-      res.json({ expired: true });
-    } else {
-      res.status(500).json(error);
-    }
-  }
-};
 
 export const verifyGoogleAuth = async (
   req: Request,
--- src/routes/token.routes.ts
+++ src/routes/token.routes.ts
@@ -1,7 +1,13 @@
-import { verifyToken } from '@/controllers/mail/token.controller';
+import {
+  verifyGoogleAuth,
+  verifyToken,
+} from '@/controllers/mail/token.controller';
 import express from 'express';
 const router = express.Router();
 
+//verification token sent via mail
 router.get('/:token', verifyToken);
 
+//verification token with google Oauth
+router.post('/token', verifyGoogleAuth);
 export default router;
